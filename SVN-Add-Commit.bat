dc3b0359785203136484b209dca3ba9e0af737eb
iCached@gmail.com <iCached@gmail.com@451616ae-e197-710b-3c2b-355724182eb8>
2011-12-20 01:55:09 +0000

    准备...

--- SVN-Add-Commit.bat
+++ SVN-Add-Commit.bat
@@ -2,6 +2,6 @@
 cd /d %~dp0
 set PATH=D:\cygwin\bin;%PATH%
 @echo on
-svn add ./SVN-Add-Commit.bat
-svn commit -m "准备..." --username xxx@gmail.com --password yyy
+rem svn add ./SVN-Add-Commit.bat
+svn commit -m "准备..." --username xxx --password yyy
 pause
